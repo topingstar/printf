0fdf2424659ac5c2eab4adbffbafb6c26d243a3b
topister onyango <topisternandera@gmail.com>
2021-07-26 15:39:47 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,52 +1,69 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
-#include <stddef.h>
+
 /**
- * _printf - recreates the printf function
- * @format: string with format specifier
+ * get_func - returns needed function
+ * @format: function identifier
+ * Return: pointer to required function
+ */
+
+static int (*get_func(const char *format))(va_list)
+{
+	unsigned int i;
+
+	print keys[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'\0', NULL}
+	};
+	for (i = 0; keys[i].id != '\0'; i++)
+	{
+		if ((keys[i].id) == *format)
+		{
+			break;
+		}
+	}
+	return (keys[i].func);
+}
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
-				i++;
-			}
+			_putchar(format[i]);
+			count++;
 		}
-		va_end(args);
-		return (count);
+		if (!format[i])
+			return (count);
+		f = get_func(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
-	return (-1);
+	va_end(valist);
+	return (count);
 }
